de0c7e49ca06d35311e843d49a15c23d4939c831
Nick Barrett <nick@edited.com>
2019-12-10 16:36:04 +0000

    Remove redundant `kubetools configs`.

--- kubetools_client/cli/__main__.py
+++ kubetools_client/cli/__main__.py
@@ -3,11 +3,7 @@
 from kubetools_client.cli import cli_bootstrap
 from kubetools_client.main import run_cli
 # Import click command groups
-from kubetools_client.cli import (  # noqa: F401, I100
-    configs,
-    deploy,
-    server,
-)
+from kubetools_client.cli import deploy, server  # noqa: F401, I100
 
 
 run_cli(cli_bootstrap)
