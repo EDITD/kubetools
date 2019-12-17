9d932c596a28731572de05c759ce280f9df3221b
Nick Barrett <nick@edited.com>
2019-12-17 11:27:51 +0000

    Add tabulate requirement.

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ if __name__ == '__main__':
             'six',
             'setuptools',
             'kubernetes',
+            'tabulate<1',
         ),
         extras_require={
             'dev': (
