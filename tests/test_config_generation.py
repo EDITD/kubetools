87aff1f646d4716819f97f1ec78ca2d565da1e67
Nick Barrett <nick@edited.com>
2020-09-14 13:36:25 +0100

    Expand config tests.
    - dependencies
    - dev overrides
    - k8s container passthrough
    - multiple deployments

--- tests/test_config_generation.py
+++ tests/test_config_generation.py
@@ -14,17 +14,36 @@ def _assert_yaml_objects(objects, yaml_filename):
     assert objects == desired_objects
 
 
+def _test_configs(folder_name, **kwargs):
+    app_dir = path.join('tests', 'configs', folder_name)
+
+    kubetools_config = load_kubetools_config(app_dir, **kwargs)
+
+    with mock.patch('kubetools.kubernetes.config.job.uuid4', lambda: 'UUID'):
+        services, deployments, jobs = generate_kubernetes_configs_for_project(
+            kubetools_config,
+        )
+
+    if services:
+        _assert_yaml_objects(services, path.join(app_dir, 'k8s_services.yml'))
+    if deployments:
+        _assert_yaml_objects(deployments, path.join(app_dir, 'k8s_deployments.yml'))
+    if jobs:
+        _assert_yaml_objects(jobs, path.join(app_dir, 'k8s_jobs.yml'))
+
+
 class TestKubernetesConfigGeneration(TestCase):
     def test_basic_app_configs(self):
-        app_dir = path.join('tests', 'configs', 'basic_app')
+        _test_configs('basic_app')
 
-        kubetools_config = load_kubetools_config(app_dir)
+    def test_dependencies_configs(self):
+        _test_configs('dependencies')
 
-        with mock.patch('kubetools.kubernetes.config.job.uuid4', lambda: 'UUID'):
-            services, deployments, jobs = generate_kubernetes_configs_for_project(
-                kubetools_config,
-            )
+    def test_dev_overrides_configs(self):
+        _test_configs('dev_overrides', dev=True)
 
-        _assert_yaml_objects(services, path.join(app_dir, 'k8s_services.yml'))
-        _assert_yaml_objects(deployments, path.join(app_dir, 'k8s_deployments.yml'))
-        _assert_yaml_objects(jobs, path.join(app_dir, 'k8s_jobs.yml'))
+    def test_k8s_container_passthrough_configs(self):
+        _test_configs('k8s_container_passthrough')
+
+    def test_multiple_deployments_configs(self):
+        _test_configs('multiple_deployments')
